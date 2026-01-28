14993339ae97f2c3a30f3af9ecd0dae7d3205286
hansa rathod <hansarathod113@gmail.com>
2026-01-28 16:54:41 +0530

    [HR] variables and datatypes added

---
+++ datatype.js
@@ -0,0 +1,4 @@
+//primitive
+let num =1;
+let num2=2;
+console.log("num",num,num2);
---
+++ variable.js
@@ -0,0 +1,11 @@
+//var
+//var a=10
+//console.log(a);
+// let
+let a=20;
+if(true)
+{
+    let a=10;
+}
+const b=3.14
+console.log(b)
